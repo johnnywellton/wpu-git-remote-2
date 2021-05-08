906541b56ace414240f0a8c463cb5b1f3354a973
johnnywellton <johnny.officialmail@yahoo.com>
2021-05-09 01:30:43 +0800

    Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cuba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur rerum ut quae beatae neque impedit quo necessitatibus, odit amet, ipsa nostrum. Sit, dolor dolorem laborum laudantium temporibus corrupti cum ea.</p>
+    <ul>
+        <li>Sandhika Galih</li>
+        <li>Doddy Ferdiansyah</li>
+        <li>Mikel</li>
+        <li>Johnny</li>
+    </ul>
+</body>
+
+</html>
